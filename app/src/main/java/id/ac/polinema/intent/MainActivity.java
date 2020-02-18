7574e6ac50e3355d0ee70adbd0942aeec76a2a94
AnjasSurya <anjasnugraha2@gmail.com>
2020-02-18 08:35:52 +0700

    Intent

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -1,15 +1,18 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+private EditText usernameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameInput = findViewById(R.id.input_username);
     }
 
 }
